fe76f46014c1b3b5a973c75ab8009df23df15fe5
YR08140921 <0814kurasan@gmail.com>
2024-03-31 20:39:41 +0900

    変更

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,3 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, passward=password)
         login(self.request, user)
         return response
-
-
-class UserProfileView(LoginRequiredMixin, TemplateView):
-    template_name = "accounts/profile.html"
